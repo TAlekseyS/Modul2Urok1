1ebd68d796dd7524ba2b9e87383004c0fac069b0
Aleksey_T <talekseys@mail.ru>
2023-07-27 00:28:18 +0300

    first version

---
+++ src/test/java/Tests.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import ru.netology.service.CashbackHackService;
+
+public class Tests {
+    @Test
+    public void firstTest() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void secondTest() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 880;
+
+        int actual = service.remain(amount);
+        int expected = 120;
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void thirstTest() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+}
